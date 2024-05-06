296259af3ca4db447906916c3198992792f8891b
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-06 23:31:15 +0700

    Get the values of class variables

--- main.py
+++ main.py
@@ -3,6 +3,29 @@ from html_document import HtmlDocument
 if __name__ == "__main__":
   # Introduction to the Python class variables
 
-  print(HtmlDocument.__name__)
-  print(type(HtmlDocument))
-  print(isinstance(HtmlDocument, type))
+  # print(HtmlDocument.__name__)
+  # print(type(HtmlDocument))
+  # print(isinstance(HtmlDocument, type))
+
+  # Get the values of class variables
+
+  # extension = HtmlDocument.extension
+  # print(extension)
+
+  # version = HtmlDocument.version
+  # print(version)
+
+  # HtmlDocument.media_type
+  # AttributeError
+
+  extension = getattr(HtmlDocument, "extension")
+  print(extension)
+
+  version = getattr(HtmlDocument, "version")
+  print(version)
+
+  # media_type = getattr(HtmlDocument, "media_type")
+  # AttributeError
+
+  media_type = getattr(HtmlDocument, "media_type", "text/html")
+  print(media_type)
